a55c7dffc5857c0cbfd2af8ab74f967af53de4a3
Freakkoe <mads.g.j@live.dk>
2023-05-07 15:57:56 +0200

    YarnCRUDv2
    Create og Delete er nu lavet - eksperimentere nu med NugetConsole for at oprette DB

--- RabbitRegister/RabbitRegister/Pages/Main/Product/CreateYarn.cshtml.cs
+++ RabbitRegister/RabbitRegister/Pages/Main/Product/CreateYarn.cshtml.cs
@@ -1,12 +1,35 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RabbitRegister.Model;
+using RabbitRegister.Services.ProductService;
 
 namespace RabbitRegister.Pages.Main.Product
 {
     public class CreateYarnModel : PageModel
     {
-        public void OnGet()
-        {
-        }
-    }
+		private IProductService _yarnService;
+
+		public CreateYarnModel(IProductService productService)
+		{
+			_yarnService = productService;
+		}
+
+		[BindProperty]
+		public Model.Yarn Yarn { get; set; }
+
+		public IActionResult OnGet()
+		{
+			return Page();
+		}
+
+		public async Task<IActionResult> OnPostAsync()
+		{
+			if (!ModelState.IsValid)
+			{
+				return Page();
+			}
+			await _yarnService.AddYarnAsync(Yarn);
+			return RedirectToPage("GetAllYarn");
+		}
+	}
 }
--- RabbitRegister/RabbitRegister/Pages/Main/Product/DeleteYarn.cshtml.cs
+++ RabbitRegister/RabbitRegister/Pages/Main/Product/DeleteYarn.cshtml.cs
@@ -1,12 +1,38 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RabbitRegister.Services.ProductService;
 
 namespace RabbitRegister.Pages.Main.Product
 {
     public class DeleteYarnModel : PageModel
     {
-        public void OnGet()
-        {
-        }
-    }
+		private IProductService _yarnService;
+
+		public DeleteYarnModel(IProductService productService)
+		{
+			_yarnService = productService;
+		}
+
+		[BindProperty]
+		public Model.Yarn Yarn { get; set; }
+
+
+		public IActionResult OnGet(int YarnId)
+		{
+			Yarn = _yarnService.GetYarn(YarnId);
+			if (Yarn == null)
+				return RedirectToPage("/NotFound");
+
+			return Page();
+		}
+
+		public async Task<IActionResult> OnPostAsync()
+		{
+			Model.Yarn deletedYarn = await _yarnService.DeleteYarnAsync(Yarn.YarnId);
+			if (deletedYarn == null)
+				return RedirectToPage("/NotFound");
+
+			return RedirectToPage("GetAllYarn");
+		}
+	}
 }
