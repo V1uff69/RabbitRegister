32157b593a77c604774ba759a37ad1a17ded185f
Viuff <kenn622r@edu.zealand.dk>
2023-05-08 10:43:37 +0200

    crud wool

---
+++ RabbitRegister/RabbitRegister/Pages/Main/Product/CreateWool.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RabbitRegister.Pages.Main.Product
+{
+    public class CreateWoolModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
