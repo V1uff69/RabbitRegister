d8663a7e806f568322d4235a02c2676608d9c908
Viuff <kenn622r@edu.zealand.dk>
2023-05-10 12:59:05 +0200

    store

--- RabbitRegister/RabbitRegister/Model/Wool.cs
+++ RabbitRegister/RabbitRegister/Model/Wool.cs
@@ -14,16 +14,19 @@ namespace RabbitRegister.Model
         [Display(Name = "Quality")]
         [Required(ErrorMessage = "Remember to add Quality")]
         public int Quality { get; set; }
+
+        public string? ImgString{ get; set; }
  
         public Wool()
         {
         }
 
-        public Wool(string productType, string productName, double weight, int quality, string color, int breederRegNo, int amount, double price) : base(productType, breederRegNo, productName, color, amount, price)
+        public Wool(string productType, string productName, double weight, int quality, string color, int breederRegNo, int amount, double price, string? imgString) : base(productType, breederRegNo, productName, color, amount, price)
         {
             Weight = weight;
             Quality = quality;
             Price = price;
+            ImgString = "/Images/Products/Wool/"+imgString;
         }
     }
 }
