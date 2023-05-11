e5e4f4ce0424deb3b9fa17ee1b9568690bee265f
Kenni Viuff Bach <kenn622r@edu.zealand.dk>
2023-05-11 13:04:01 +0200

    product page

---
+++ RabbitRegister/RabbitRegister/Pages/Main/Product/ProductInfo.cshtml.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RabbitRegister.Model;
+using RabbitRegister.Services.ProductService;
+
+namespace RabbitRegister.Pages.Main.Product
+{
+    public class ProductInfoModel : PageModel
+    {
+        private IProductService _productService { get; set; }
+        public List<Model.Wool> Wools { get; set; }
+        public List<Model.Yarn> Yarns { get; set; }
+        public Wool wool { get; set; }
+        public Yarn yarn { get; set; }
+
+        public ProductInfoModel(IProductService productService)
+        {
+            _productService = productService;
+        }
+        public IActionResult OnGet(int Id)
+        {
+            if (yarn.ProductType == "Yarn")
+            {
+                yarn = _productService.GetYarn(Id);
+            }
+            else { 
+                if (wool.ProductType == "Wool")
+                wool = _productService.GetWools(Id);
+            }
+            return Page();
+        }
+    }
+}
