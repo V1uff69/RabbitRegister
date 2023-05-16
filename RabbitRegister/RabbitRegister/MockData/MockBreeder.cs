d39446dea1f87b84fe9e296de4471c99fb71ffe4
Mikfri <mikk908w@edu.zealand.dk>
2023-05-16 20:43:18 +0200

    d

--- RabbitRegister/RabbitRegister/MockData/MockBreeder.cs
+++ RabbitRegister/RabbitRegister/MockData/MockBreeder.cs
@@ -9,7 +9,7 @@ namespace RabbitRegister.MockData
 
         public static List<Breeder> breeders = new List<Breeder>()
         {
-            new Breeder(5095,"Ida","Fynsvej __",4060,"Ida.fri.@gmail.com","27586455",passwordHasher.HashPassword(null, "Ida123"),true) {},
+            new Breeder(5095,"Ida","Fynsvej 14",4060,"Ida.fri.@gmail.com","27586455",passwordHasher.HashPassword(null, "Ida123"),true) {},
             new Breeder(5053,"Maja","Holbækvej __",4300,"MajaHulstroem.@gmail.com","28733085",passwordHasher.HashPassword(null, "Maja123"),false) {}
         };
         public static List<Breeder> GetMockBreeders() { return breeders; }
