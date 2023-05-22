06fd3360efb4dbf1cfd7b966d0b192a9173b1e79
Kajebabz <kqligkaj14@msn.com>
2023-05-22 12:19:21 +0200

    hgc

--- RabbitRegister/RabbitRegister/Migrations/20230522095346_Rab1.Designer.cs
+++ RabbitRegister/RabbitRegister/Migrations/20230522095346_Rab1.Designer.cs
@@ -12,7 +12,7 @@ using RabbitRegister.EFDbContext;
 namespace RabbitRegister.Migrations
 {
     [DbContext(typeof(ItemDbContext))]
-    [Migration("20230521123330_Rab1")]
+    [Migration("20230522095346_Rab1")]
     partial class Rab1
     {
         /// <inheritdoc />
