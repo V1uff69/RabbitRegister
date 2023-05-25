c74b28bc03cc400d8a58302862bef15a8f2ea35c
Mikfri <mikk908w@edu.zealand.dk>
2023-05-25 09:09:12 +0200

    Filterfunktion på GetAllRabbit siden (sidebar)

--- RabbitRegister/RabbitRegister/Pages/Main/Rabbit/GetAllRabbits.cshtml.cs
+++ RabbitRegister/RabbitRegister/Pages/Main/Rabbit/GetAllRabbits.cshtml.cs
@@ -26,9 +26,6 @@ namespace RabbitRegister.Pages.Main.Rabbit
         [BindProperty]
         public int MinRating { get; set; }
 
-        [BindProperty]
-        public int MaxRating { get; set; }
-
 
         public IActionResult OnGet(int breederRegNo, string action)
         {
@@ -43,11 +40,7 @@ namespace RabbitRegister.Pages.Main.Rabbit
             else if (action == "NotOwnedRabbitsWithMyBreederRegNo")
             {
                 Rabbits = _rabbitService.GetNotOwnedRabbitsWithMyBreederRegNo(breederRegNo);
-            }
-            //else if (action == "AllRabbits")
-            //{
-            //    Rabbits = _rabbitService.GetAllRabbits(id, breederRegNo);
-            //}
+            }           
             else
             {
                 Rabbits = _rabbitService.GetOwnedAliveRabbits(breederRegNo);
@@ -57,52 +50,12 @@ namespace RabbitRegister.Pages.Main.Rabbit
         }
 
 
-        public IActionResult OnGetSortById()
-        {
-            Rabbits = _rabbitService.SortById().ToList();
-            return Page();
-        }
-
-        public IActionResult OnGetSortByIdDescending()
-        {
-            Rabbits = _rabbitService.SortByIdDescending().ToList();
-            return Page();
-        }
-
-        public IActionResult OnGetSortByName()
-        {
-            Rabbits = _rabbitService.SortByName().ToList();
-            return Page();
-        }
-
-        public IActionResult OnGetSortByNameDescending()
-        {
-            Rabbits = _rabbitService.SortByNameDescending().ToList();
-            return Page();
-        }
-
-        public IActionResult OnGetSortByRating()
-        {
-            Rabbits = _rabbitService.SortByRating().ToList();
-            return Page();
-        }
-
-        public IActionResult OnGetSortByRatingDescending()
-        {
-            Rabbits = _rabbitService.SortByRatingDescending().ToList();
-            return Page();
-        }
-
         public IActionResult OnPostNameSearch()
         {
             Rabbits = _rabbitService.NameSearch(SearchString).ToList();
             return Page();
         }
 
-        public IActionResult OnPostRatingFilter()
-        {
-            Rabbits = _rabbitService.RatingFilter(MaxRating, MinRating).ToList();
-            return Page();
-        }
+  
     }
 }
